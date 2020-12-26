b72a9a3aaea06b9a6bc82facb3f210fcc3f908d8
Parocq <55479321+Parocq@users.noreply.github.com>
2020-12-26 17:05:57 +0300

    Initial commit; Liquibase added; created tables: Users, user_role,roles.

---
+++ src/main/java/com/bsuir/herman/demo/model/domain/GameMode.java
@@ -0,0 +1,6 @@
+package com.bsuir.herman.demo.model.domain;
+
+public enum GameMode {
+    WhoFirst,
+    SaperMine
+}
---
+++ src/test/java/com/bsuir/herman/demo/SaperServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsuir.herman.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaperServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
